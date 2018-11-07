9d45e5ada7c8e1fcab0ca5b3179227a138dee42d
amulx <648303666@qq.com>
2018-11-07 11:55:16 +0800

    增加多语句触发器
    增加多语句触发器

--- data/mysql_trigger.sql
+++ data/mysql_trigger.sql
@@ -5,3 +5,23 @@ WHERE
 	topicid = OLD.topicid;
   
 # 当贴子删除时，自动删除该贴在收藏表的的所有记录
+
+DELIMITER ||
+
+CREATE TRIGGER topic_delete AFTER DELETE ON tbl_topic FOR EACH ROW
+BEGIN
+	DELETE
+FROM
+	tbl_collect
+WHERE
+	topicid = OLD.topicid;
+
+DELETE
+FROM
+	tbl_consume
+WHERE
+	topicid = OLD.topicid;
+END
+||
+
+DELIMITER ;
